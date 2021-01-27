cf4b4464570bbc9a1ef9ef2b4c43ddaa7fe7152b
Julianna Piskorz <julianna.piskorz19@imperial.ac.uk>
2021-01-27 12:38:02 +0000

    Adding equality method to the Polynomial class.

--- polynomials/polynomials.py
+++ polynomials/polynomials.py
@@ -22,4 +22,11 @@ class Polynomial:
         
         return " + ".join(reversed(terms)) or "0"
         
+    def __repr__(self):
+
+        return self.__class__.__name__ + "(" + repr(self.coefficients) + ")"
+    
+    def __eq__(self, other):
+
+        return isinstance(other, Polynomial) and self.coefficients == other.coefficients
 
--- tests/test_polynomials.py
+++ tests/test_polynomials.py
@@ -4,3 +4,6 @@ def test_print():
     p = Polynomial((1, 3, 0, 5, 1))
     q = Polynomial((0,))
     assert str(p) == "x^4 + 5x^3 + 3x + 1" and str(q) == "0"
+
+def test_equality():
+    assert Polynomial((0, 1)) == Polynomial((0,1))
